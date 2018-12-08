9d5cf29656982731dfbfefc66cced5195c35b996
rav97 <35076475+rav97@users.noreply.github.com>
2018-12-08 12:59:24 +0100

    Add files via upload
    All project

---
+++ PermCipher/PermCipher/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace PermCipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.input = new System.Windows.Forms.RichTextBox();
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.output = new System.Windows.Forms.RichTextBox();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.opFile = new System.Windows.Forms.Button();
+            this.FileExport = new System.Windows.Forms.Button();
+            this.cipher = new System.Windows.Forms.CheckBox();
+            this.fromFile = new System.Windows.Forms.CheckBox();
+            this.manually = new System.Windows.Forms.CheckBox();
+            this.decipher = new System.Windows.Forms.CheckBox();
+            this.key = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(12, 243);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(518, 39);
+            this.start.TabIndex = 0;
+            this.start.Text = "START";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(12, 67);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(518, 80);
+            this.input.TabIndex = 3;
+            this.input.Text = "";
+            this.input.TextChanged += new System.EventHandler(this.input_TextChanged);
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.AutoSize = true;
+            this.inputLabel.Location = new System.Drawing.Point(10, 51);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(60, 13);
+            this.inputLabel.TabIndex = 4;
+            this.inputLabel.Text = "Podaj tekst";
+            this.inputLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(12, 301);
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.Size = new System.Drawing.Size(518, 85);
+            this.output.TabIndex = 5;
+            this.output.Text = "";
+            this.output.TextChanged += new System.EventHandler(this.output_TextChanged);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(12, 285);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(59, 13);
+            this.outputLabel.TabIndex = 6;
+            this.outputLabel.Text = "Szyfrogram";
+            this.outputLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // opFile
+            // 
+            this.opFile.Location = new System.Drawing.Point(397, 8);
+            this.opFile.Name = "opFile";
+            this.opFile.Size = new System.Drawing.Size(75, 23);
+            this.opFile.TabIndex = 7;
+            this.opFile.Text = "Wybierz plik";
+            this.opFile.UseVisualStyleBackColor = true;
+            this.opFile.Click += new System.EventHandler(this.opFile_Click);
+            // 
+            // FileExport
+            // 
+            this.FileExport.Location = new System.Drawing.Point(13, 394);
+            this.FileExport.Name = "FileExport";
+            this.FileExport.Size = new System.Drawing.Size(75, 23);
+            this.FileExport.TabIndex = 10;
+            this.FileExport.Text = "Zapisz do pliku";
+            this.FileExport.UseVisualStyleBackColor = true;
+            this.FileExport.Click += new System.EventHandler(this.FileExport_Click);
+            // 
+            // cipher
+            // 
+            this.cipher.AutoSize = true;
+            this.cipher.Location = new System.Drawing.Point(209, 207);
+            this.cipher.Name = "cipher";
+            this.cipher.Size = new System.Drawing.Size(57, 17);
+            this.cipher.TabIndex = 11;
+            this.cipher.Text = "Szyfruj";
+            this.cipher.UseVisualStyleBackColor = true;
+            this.cipher.CheckedChanged += new System.EventHandler(this.cipher_CheckedChanged);
+            // 
+            // fromFile
+            // 
+            this.fromFile.AutoSize = true;
+            this.fromFile.Location = new System.Drawing.Point(12, 8);
+            this.fromFile.Name = "fromFile";
+            this.fromFile.Size = new System.Drawing.Size(133, 17);
+            this.fromFile.TabIndex = 12;
+            this.fromFile.Text = "Wybieram dane z pliku";
+            this.fromFile.UseVisualStyleBackColor = true;
+            this.fromFile.CheckedChanged += new System.EventHandler(this.fromFile_CheckedChanged);
+            // 
+            // manually
+            // 
+            this.manually.AutoSize = true;
+            this.manually.Location = new System.Drawing.Point(12, 31);
+            this.manually.Name = "manually";
+            this.manually.Size = new System.Drawing.Size(173, 17);
+            this.manually.TabIndex = 13;
+            this.manually.Text = "Wprowadzę dane samodzielnie";
+            this.manually.UseVisualStyleBackColor = true;
+            this.manually.CheckedChanged += new System.EventHandler(this.manually_CheckedChanged);
+            // 
+            // decipher
+            // 
+            this.decipher.AutoSize = true;
+            this.decipher.Location = new System.Drawing.Point(272, 207);
+            this.decipher.Name = "decipher";
+            this.decipher.Size = new System.Drawing.Size(69, 17);
+            this.decipher.TabIndex = 2;
+            this.decipher.Text = "Deszyfruj";
+            this.decipher.UseVisualStyleBackColor = true;
+            this.decipher.CheckedChanged += new System.EventHandler(this.decipher_CheckedChanged);
+            // 
+            // key
+            // 
+            this.key.Location = new System.Drawing.Point(12, 172);
+            this.key.Name = "key";
+            this.key.Size = new System.Drawing.Size(518, 20);
+            this.key.TabIndex = 14;
+            this.key.TextChanged += new System.EventHandler(this.key_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Klucz";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 429);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.key);
+            this.Controls.Add(this.manually);
+            this.Controls.Add(this.fromFile);
+            this.Controls.Add(this.cipher);
+            this.Controls.Add(this.FileExport);
+            this.Controls.Add(this.opFile);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.inputLabel);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.decipher);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Szyfr";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.RichTextBox input;
+        private System.Windows.Forms.Label inputLabel;
+        private System.Windows.Forms.RichTextBox output;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button opFile;
+        private System.Windows.Forms.Button FileExport;
+        private System.Windows.Forms.CheckBox cipher;
+        private System.Windows.Forms.CheckBox fromFile;
+        private System.Windows.Forms.CheckBox manually;
+        private System.Windows.Forms.CheckBox decipher;
+        private System.Windows.Forms.TextBox key;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PermCipher/PermCipher/Form1.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PermCipher
+{
+    public partial class Form1 : Form
+    {
+        public string inputText, filepath, exportPath, klucz;
+        private bool readFile;
+ 
+        public Form1()
+        {
+            InitializeComponent();
+            manually.CheckState = CheckState.Checked;
+            cipher.CheckState = CheckState.Checked;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void input_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void cipher_CheckedChanged(object sender, EventArgs e)
+        {
+            switch (cipher.CheckState)
+            {
+                case CheckState.Checked:
+                    decipher.CheckState = CheckState.Unchecked;
+                    break;
+                case CheckState.Unchecked:
+                    decipher.CheckState = CheckState.Checked;
+                    break;
+            }
+        }
+        private void decipher_CheckedChanged(object sender, EventArgs e)
+        {
+            switch(decipher.CheckState)
+            {
+                case CheckState.Checked:
+                    cipher.CheckState = CheckState.Unchecked;
+                    break;
+                case CheckState.Unchecked:
+                    cipher.CheckState = CheckState.Checked;
+                    break;
+            }
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            if(cipher.CheckState != CheckState.Unchecked || decipher.CheckState != CheckState.Unchecked)
+            {
+                if (key.Text == "")
+                    MessageBox.Show("Nie podano klucza");
+                else
+                {
+                    if (key.Text.Length > 9)
+                        MessageBox.Show("Za długi klucz!\nMaksymalna dlugosc klucza: 9");
+                    else
+                    {
+                        if (keyCheck(key.Text))
+                        {
+                            klucz = key.Text;
+                            inputText = input.Text;
+                            if (cipher.CheckState == CheckState.Checked)
+                            {
+                                if(InputCheck(inputText))
+                                    output.Text = szyfruj(inputText, klucz);
+                                else
+                                    MessageBox.Show("Niedozwolone znaki na wejściu.\nDozwolone znaki:\n-Alfabet angielski\n-Znaki specjalne");
+                            }
+                            if(decipher.CheckState==CheckState.Checked)
+                            {
+                                if (InputCheck(inputText))
+                                    output.Text = deszyfruj(inputText, klucz);
+                                else
+                                    MessageBox.Show("Niedozwolone znaki na wejściu.\nDozwolone znaki:\n-Alfabet angielski\n-Znaki specjalne");
+                            }
+                        }
+                        else
+                            MessageBox.Show("Nieprawidłowy klucz!");
+                    }
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Najpierw wybierz czynność!");
+            }
+        }
+
+        private void output_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void opFile_Click(object sender, EventArgs e)
+        {
+            fromFile.CheckState = CheckState.Checked;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if(openFileDialog1.ShowDialog()==System.Windows.Forms.DialogResult.OK)
+            {
+                filepath = openFileDialog1.FileName;
+                //MessageBox.Show(filepath);
+            }
+            if (filepath != null)
+            {
+                if (readFile)
+                {
+                    FileStream fs = new FileStream(filepath, FileMode.Open, FileAccess.Read);
+                    try
+                    {
+                        StreamReader sr = new StreamReader(fs);
+                        inputText = sr.ReadToEnd();
+                        input.Text = inputText;
+                        sr.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+                }
+            }
+        }
+
+        private void FileExport_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            if(saveFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                using (Stream s = File.Open(saveFileDialog1.FileName, FileMode.CreateNew))
+                using (StreamWriter sw = new StreamWriter(s))
+                {
+                    sw.Write(output.Text);
+                    MessageBox.Show("Zapisano do pliku:\n"+saveFileDialog1.FileName);
+                    sw.Close();
+                }
+            }
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void key_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fromFile_CheckedChanged(object sender, EventArgs e)
+        {
+            switch (fromFile.CheckState)
+            {
+                case CheckState.Checked:
+                    opFile.Enabled = true;
+                    input.Enabled = false;
+                    readFile = true;
+                    manually.CheckState = CheckState.Unchecked;
+                    break;
+                case CheckState.Unchecked:
+                    opFile.Enabled = false;
+                    input.Enabled = true;
+                    readFile = false;
+                    manually.CheckState = CheckState.Checked;
+                    break;
+            }
+        }
+
+        private void manually_CheckedChanged(object sender, EventArgs e)
+        {
+            switch (manually.CheckState)
+            {
+                case CheckState.Checked:
+                    opFile.Enabled = false;
+                    input.Enabled = true;
+                    readFile = false;
+                    fromFile.CheckState = CheckState.Unchecked;
+                    break;
+                case CheckState.Unchecked:
+                    opFile.Enabled = true;
+                    input.Enabled = false;
+                    readFile = true;
+                    fromFile.CheckState = CheckState.Checked;
+                    break;
+            }
+        }
+        private bool keyCheck(string key)
+        {
+            bool tn=true;
+            for (int i = 0; i < key.Length; i++)
+            {
+                if ((int)key[i] < 48 || (int)key[i] > 57)
+                {
+                    tn = false;
+                    break;
+                }
+            }
+            if(tn)
+            {
+                string keyValid = "123456789";
+                string s1 = keyValid.Substring(0, key.Length);
+                string s2 = key;
+                s2 = String.Concat(s2.OrderBy(c => c));
+                if (s1 != s2)
+                    tn = false;
+            }
+            return tn;
+        }
+        private int[] keyElem(string key)
+        {
+            int[] tab = new int[key.Length];
+            for (int i = 0; i < key.Length; i++)
+            {
+                tab[i] = (int)key[i] - 49;
+            }
+            return tab;
+        }
+        private string szyfruj(string txt, string klucz)
+        {
+            string szyfr = "";
+            int dl_blok = klucz.Length;
+            txt = normalizuj(txt, dl_blok);
+            int il_blok = txt.Length / dl_blok;
+            int[] tab = new int[dl_blok];
+            tab = keyElem(klucz);
+            char[,] znaki = przepisz(txt, dl_blok, il_blok);
+            for (int i = 0; i < il_blok; i++)
+            {
+                foreach (int k in tab)
+                    szyfr += znaki[k, i];
+            }
+            return szyfr;
+        }
+        private string deszyfruj(string txt, string klucz)
+        {
+            string deszyfr = "";
+            int dl_blok = klucz.Length;
+            txt = normalizuj(txt, dl_blok);
+            int il_blok = txt.Length / dl_blok;
+            int[] tab = new int[dl_blok];
+            tab = keyElem(klucz);
+            tab = permOdwr(tab);
+            char[,] znaki = przepisz(txt, dl_blok, il_blok);
+            for (int i = 0; i < il_blok; i++)
+            {
+                foreach (int k in tab)
+                    deszyfr += znaki[k, i];
+            }
+            return deszyfr;
+        }
+        private string normalizuj(string txt, int dl)
+        {
+            int dop = dl - (txt.Length % dl);
+            if (dop != dl)
+                for (int i = 0; i < dop; i++)
+                {
+                    txt += ' ';
+                }
+            return txt;
+        }
+        private char[,] przepisz(string txt, int wier, int kol)
+        {
+            char[,] tab = new char[wier, kol];
+            for (int i = 0; i < kol; i++)
+            {
+                for (int j = 0; j < wier; j++)
+                {
+                    int przelicz = ((wier - 1) * i) + (j + i);
+                    tab[j, i] = txt[przelicz];
+                }
+            }
+            return tab;
+        }
+        private int[] permOdwr(int[] tab)
+        {
+            int[] tab2 = new int[tab.Length];
+            for (int i = 0; i < tab.Length; i++)
+            {
+                for (int j = 0; j < tab.Length; j++)
+                {
+                    if (i == tab[j])
+                        tab2[i] = j;
+                }
+                Console.WriteLine(tab2[i]);
+            }
+            return tab2;
+        }
+        private bool InputCheck(string input)
+        {
+            bool tn = true;
+            for(int i=0;i<input.Length;i++)
+            {
+                if((int)input[i]<32 || (int)input[i]>126)
+                {
+                    if ((int)input[i] != 13 && (int)input[i] != 10)
+                    {
+                        tn = false;
+                        break;
+                    }
+                }
+                if (input[i] >= '0' && input[i] <= '9')
+                {
+                    tn = false;
+                    break;
+                }
+            }
+            return tn;
+        }
+    }
+}
